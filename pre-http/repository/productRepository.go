1635617e98939a411f1a3385e300313741369a41
Ashish <juyal.ashish@gmail.com>
2022-08-24 22:58:01 +0530

    go generate for mocks

--- pre-http/repository/productRepository.go
+++ pre-http/repository/productRepository.go
@@ -2,6 +2,8 @@ package repository
 
 import "pre-http/model"
 
+//go:generate mockgen -destination=../mocks/repository/mockProductRepository.go -package=repository pre-http/repository ProductRepository
+
 type ProductRepository interface {
 	FindBy(id int) *model.Product
 	FindAll() []model.Product
