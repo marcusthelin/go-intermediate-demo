a5b8357c0685b205b347fe9c2c814dc8c46e57f2
Ashish <juyal.ashish@gmail.com>
2022-08-26 15:35:58 +0530

    route test

---
+++ pre-http/app/productHandler_test.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"pre-http/repository"
+	"pre-http/service"
+	"testing"
+)
+
+func Test_get_all_products(t *testing.T) {
+	repo := repository.NewInMemoryProductRepo()
+	productService := service.NewProductService(repo)
+	pr := ProductRoute{service: *productService}
+
+	//router := mux.NewRouter()
+	//router.HandleFunc("/products", pr.getAllProducts)
+
+	recorder := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/products", nil)
+
+	//router.ServeHTTP(recorder, req)
+
+	pr.getAllProducts(recorder, req)
+
+	if recorder.Code != http.StatusOK {
+		t.Error("unexpected status code")
+	}
+}
